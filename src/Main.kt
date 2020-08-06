5e0b8d438a50d6d901c9b371b30179fecca24f72
ythosa <vasus714@yandex.ru>
2020-08-06 21:19:33 +0300

    Energy calculation functions add

--- src/Main.kt
+++ src/Main.kt
@@ -5,12 +5,97 @@ import java.awt.image.BufferedImage;
 import java.io.File
 import java.util.*
 import javax.imageio.ImageIO
-
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
     val inName = getArgValue(args, "-in")
     val outName = getArgValue(args, "-out")
+    energyImageConverter(inName, outName)
+}
 
+private fun energyImageConverter(inName: String?, outName: String?) {
+    if (inName != null && outName != null) {
+        val image = createImage(inName)
+        convertToEnergy(image)
+        createImageFile(image, outName)
+    }
+}
+
+fun convertToEnergy(image: BufferedImage) {
+    for (i in 0 until image.width) {
+        for (j in 0 until image.height) {
+            val color = Color(image.getRGB(i, j))
+//            val negative = Color(
+//                    255 - color.red,
+//                    255 - color.green,
+//                    255 - color.blue
+//            )
+
+//            image.setRGB(i, j, negative.rgb)
+        }
+    }
+}
+
+fun getEnergyArrayOfImage(image: BufferedImage): Array<DoubleArray> {
+    val energyArrayOfImage = Array(image.width) { DoubleArray(image.height) }
+    for (i in 0 until image.width) {
+        for (j in 0 until image.height) {
+            val posX = when (i) {
+                0 -> 1
+                image.width - 1 -> image.width - 2
+                else -> i
+            }
+            val posY = when (j) {
+                0 -> 1
+                image.height - 1 -> image.height - 2
+                else -> j
+            }
+
+            val colorLeftX = Color(image.getRGB(posX - 1, posY))
+            val colorRightX = Color(image.getRGB(posX + 1, posY))
+            val gradientX = (colorLeftX.red.toDouble() - colorRightX.red).pow(2.0) +
+                    (colorLeftX.green.toDouble() - colorRightX.green).pow(2.0) +
+                    (colorLeftX.blue.toDouble() - colorRightX.blue).pow(2.0)
+
+            val colorTopY = Color(image.getRGB(posX, posY - 1))
+            val colorDownY = Color(image.getRGB(posX, posY + 1))
+            val gradientY = (colorTopY.red.toDouble() - colorDownY.red).pow(2.0) +
+                    (colorTopY.green.toDouble() - colorDownY.green).pow(2.0) +
+                    (colorTopY.blue.toDouble() - colorDownY.blue).pow(2.0)
+
+            energyArrayOfImage[i][j] = sqrt(gradientX + gradientY)
+        }
+    }
+    return energyArrayOfImage
+}
+
+fun getMaxEnergyValueOfImage(energyArrayOfImage: Array<DoubleArray>): Double {
+    var maxValue = 0.0
+    for (i in energyArrayOfImage.indices) {
+        for (j in energyArrayOfImage[i].indices) {
+            if (energyArrayOfImage[i][j] > maxValue) {
+                maxValue = energyArrayOfImage[i][j]
+            }
+        }
+    }
+    return maxValue
+}
+
+fun normalizeEnergyImage(image: BufferedImage) {
+
+}
+
+fun normalizeEnergyOfPixel(energy: Double, maxEnergyValue: Double): Int {
+    return (255.0 * energy / maxEnergyValue).toInt()
+}
+
+fun getNormalizedEnergyPixel(energy: Double, maxEnergyValue: Double): Color {
+    val intensity = normalizeEnergyOfPixel(energy, maxEnergyValue)
+    return Color(intensity, intensity, intensity)
+}
+
+private fun toNegativeImageConverter(inName: String?, outName: String?) {
     if (inName != null && outName != null) {
         val image = createImage(inName)
         convertToNegative(image)
@@ -52,7 +137,7 @@ private fun createImageFile(image: BufferedImage, fileName: String) {
     ImageIO.write(image, "png", file)
 }
 
-fun drawCrossedOutRectangle() {
+private fun drawCrossedOutRectangle() {
     val scanner = Scanner(System.`in`)
 
     val (width, height) = getRectangleSize(scanner)
@@ -71,7 +156,7 @@ fun drawCrossedOutRectangle() {
     ImageIO.write(img, "png", File(outputPath))
 }
 
-fun getRectangleSize(scanner: Scanner): Pair<Int, Int> {
+private fun getRectangleSize(scanner: Scanner): Pair<Int, Int> {
     println("Enter rectangle width:")
     val w = scanner.nextInt()
 
@@ -81,7 +166,7 @@ fun getRectangleSize(scanner: Scanner): Pair<Int, Int> {
     return Pair(w, h)
 }
 
-fun getImageName(scanner: Scanner): String {
+private fun getImageName(scanner: Scanner): String {
     println("Enter output image name:")
     return scanner.next()
 }
