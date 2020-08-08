de365681c7b87a8b630f88ac1804deb312fda445
ythosa <vasus714@yandex.ru>
2020-08-08 21:57:54 +0300

    Update help command

--- src/Main.kt
+++ src/Main.kt
@@ -6,9 +6,11 @@ import seamcarving.workers.InputWorker
 fun main(args: Array<String>) {
     val inputWorker = InputWorker(args)
 
+
+
     val actionType = inputWorker.getActionType()
     val inPath = inputWorker.getInputPath()
     val outPath = inputWorker.getOutputPath()
 
-    ActionWorker(actionType, inPath, outPath).start()
+    ActionWorker(actionType, inPath, outPath).parse()
 }
--- src/workers/ActionWorker.kt
+++ src/workers/ActionWorker.kt
@@ -4,7 +4,7 @@ import seamcarving.coverters.EnergyConverter
 import seamcarving.coverters.NegativeConverter
 
 class ActionWorker(private val name: String?, private val inPath: String?, private val outPath: String?) {
-    fun start() {
+    fun parse() {
         when (name) {
             null -> help()
             "energy" -> energyConverting()
@@ -13,19 +13,26 @@ class ActionWorker(private val name: String?, private val inPath: String?, priva
     }
 
     private fun help() {
-        println("its help")
+        println("Welcome to SeamCarving.\n" +
+                "Supported flags:\n" +
+                "\t-action //- type of work with image, which could be:\n" +
+                "\t\t+ energy //- convert image from -in path to -out path with energy filter\n" +
+                "\t\t+ negative //- convert image from -in path to -out path with negative filter\n" +
+                "\t\t+ crossed-rec //- create crossed rectangle image\n" +
+                "\t-in //- input path of existing image\n" +
+                "\t-out //- output path of created image")
     }
 
     private fun energyConverting() {
-        val converter = EnergyConverter()
         if (inPath != null && outPath != null) {
+            val converter = EnergyConverter()
             converter.createConverted(inPath, outPath)
         }
     }
 
     private fun negativeConverting() {
-        val converter = NegativeConverter()
         if (inPath != null && outPath != null) {
+            val converter = NegativeConverter()
             converter.createConverted(inPath, outPath)
         }
     }
