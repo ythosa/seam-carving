0877cfc202995719924c866f4202266f9d1b9592
ythosa <vasus714@yandex.ru>
2020-08-09 22:08:49 +0300

    Small fix

--- src/Main.kt
+++ src/Main.kt
@@ -6,8 +6,6 @@ import seamcarving.workers.InputWorker
 fun main(args: Array<String>) {
     val inputWorker = InputWorker(args)
 
-
-
     val actionType = inputWorker.getActionType()
     val inPath = inputWorker.getInputPath()
     val outPath = inputWorker.getOutputPath()
